4b13db3ef7e5fe5c8e79d4582cdf696d354ee741
suggoooiii <ramighammami@gmail.com>
2024-02-21 07:35:35 +0200

    Add Leva controls for fog settings

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,70 @@
 import { Canvas } from "@react-three/fiber";
-import Experience from "./components/Experience";
 import "./App.css";
+import { useEffect, useRef } from "react";
+import { folder, Leva, useControls } from "leva";
+import Experience from "./components/Experience";
 
 function App() {
+  const ref = useRef();
+  useEffect(() => {
+    console.log("🚀 ~ useEffect ~ ref.current:", ref.current);
+  }, []);
+
+  const Opts = useControls({
+    Fog: folder({
+      Fog_enbled: {
+        label: "Enable",
+        value: true,
+      },
+      Fog_color: {
+        label: "Color",
+        value: "#ffffff",
+      },
+      Fog_density: {
+        label: "Density",
+        min: 0,
+        max: 1,
+        step: 0.01,
+        value: 0.3,
+      },
+      Fog_speed: {
+        label: "Speed",
+        min: 0,
+        max: 10,
+        step: 0.1,
+        value: 1.7,
+      },
+      Fog_distortion: {
+        label: "Distortion",
+        min: 0,
+        max: 2,
+        step: 0.01,
+        value: 1.3,
+      },
+      Fog_scale: {
+        label: "Scale",
+        min: 0,
+        max: 100,
+        step: 1,
+        value: [100, 2.5, 100],
+      },
+      Fog_position: {
+        label: "Position",
+        min: 0,
+        max: 100,
+        step: 1,
+        value: [0, 0, 0],
+      },
+    }),
+    AutoRotate: {
+      value: true,
+    },
+  });
+
   return (
     <Canvas shadows camera={{ position: [0, 3, 9], fov: 42 }}>
-      <color attach="background" args={["#15151a"]} />
+      <color attach="background" args={["#20ef46"]} />
+      <fogExp2 ref={ref} density={Opts.Fog_density} color={Opts.Fog_color} />
       <Experience />
     </Canvas>
   );
--- frontend/src/components/Experience.jsx
+++ frontend/src/components/Experience.jsx
@@ -52,57 +52,6 @@ function Buildings() {
   );
 }
 function Experience() {
-  const Opts = useControls({
-    Fog: folder({
-      Fog_enbled: {
-        label: "Enable",
-        value: true,
-      },
-      Fog_color: {
-        label: "Color",
-        value: "#ffffff",
-      },
-      Fog_density: {
-        label: "Density",
-        min: 0,
-        max: 1,
-        step: 0.01,
-        value: 0.3,
-      },
-      Fog_speed: {
-        label: "Speed",
-        min: 0,
-        max: 10,
-        step: 0.1,
-        value: 1.7,
-      },
-      Fog_distortion: {
-        label: "Distortion",
-        min: 0,
-        max: 2,
-        step: 0.01,
-        value: 1.3,
-      },
-      Fog_scale: {
-        label: "Scale",
-        min: 0,
-        max: 100,
-        step: 1,
-        value: [100, 2.5, 100],
-      },
-      Fog_position: {
-        label: "Position",
-        min: 0,
-        max: 100,
-        step: 1,
-        value: [0, 0, 0],
-      },
-    }),
-
-    AutoRotate: {
-      value: true,
-    },
-  });
   return (
     <>
       <OrbitControls minPolarAngle={0} maxPolarAngle={Math.PI / 2} />
