b371ae04fce722f396d9583d0b1b97a13b2c6659
suggoooiii <ramighammami@gmail.com>
2024-02-24 10:25:39 +0200

    createInstanced Geo fn added

--- frontend/src/components/Experience.jsx
+++ frontend/src/components/Experience.jsx
@@ -3,11 +3,48 @@ import { Cyborg } from "./Cyborg";
 import { Center, Environment, OrbitControls } from "@react-three/drei";
 import Lights from "./Lights";
 import { Floor } from "./Floor";
-import { Color, MeshPhysicalMaterial } from "three";
+import * as THREE from "three";
 import { folder, Leva, useControls } from "leva";
 import { useFrame, useThree } from "@react-three/fiber";
 import { Buildings } from "./Buildings";
 
+const createInstancedGeometry = (geometry) => {
+  let instances = 800;
+
+  let instancedGeometry = new THREE.InstancedBufferGeometry();
+  instancedGeometry.copy(geometry);
+  instancedGeometry.instanceCount = instances;
+
+  let aInstance = new Float32Array(4 * instances);
+  let aInstance2 = new Float32Array(4 * instances);
+
+  for (let i = 0; i < instances; i++) {
+    // Circle angle
+    aInstance[i * 4 + 0] = Math.random() * Math.PI * 2;
+    // Torus tube angle
+    aInstance[i * 4 + 1] = Math.random() * Math.PI * 2;
+    // Torus tube scale
+    aInstance[i * 4 + 2] = Math.random() * 2;
+    // Scale
+    aInstance[i * 4 + 3] = 0.2 + Math.random() * Math.random();
+
+    // Rotation
+    aInstance2[i * 4 + 0] = Math.random() * Math.PI * 2;
+
+    aInstance2[i * 4 + 1] = Math.floor(Math.random() * 3);
+  }
+
+  instancedGeometry.setAttribute(
+    "aInstance",
+    new THREE.InstancedBufferAttribute(aInstance, 4, false)
+  );
+  instancedGeometry.setAttribute(
+    "aInstance2",
+    new THREE.InstancedBufferAttribute(aInstance2, 4, false)
+  );
+
+  return instancedGeometry;
+};
 function Experience() {
   const Opts = useControls({
     Fog: folder({
