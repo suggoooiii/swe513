5b726a52c9bbf131e3d6171225e81fdf1b8dddd0
ikigai <ramighammami@gmail.com>
2024-03-05 02:11:10 +0200

    tracking

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,13 +4,61 @@ import { useEffect, useRef } from "react";
 import Experience from "./components/Experience";
 import { OrbitControls } from "@react-three/drei";
 import * as THREE from "three";
+import { useControls } from "leva";
+
+// eg for instancing meshes with r3f
+{
+  /* <Bvh firstHitOnly> */
+}
+{
+  /* <Shoes data={data} range={range} /> */
+}
+{
+  /* </Bvh> */
+}
+
+// function Shoes({ data, range }) {
+//   const { nodes, materials } = useGLTF('/shoe.glb')
+//   return (
+//     <Instances range={range} material={materials.phong1SG} geometry={nodes.Shoe.geometry}>
+//       {data.map((props, i) => (
+//         <Shoe key={i} {...props} />
+//       ))}
+//     </Instances>
+//   )
+// }
+
+// function Shoe({ random, color = new THREE.Color(), ...props }) {
+//   const ref = useRef()
+//   const [hovered, setHover] = useState(false)
+//   useFrame((state) => {
+//     const t = state.clock.getElapsedTime() + random * 10000
+//     ref.current.rotation.set(Math.cos(t / 4) / 2, Math.sin(t / 4) / 2, Math.cos(t / 1.5) / 2)
+//     ref.current.position.y = Math.sin(t / 1.5) / 2
+//     ref.current.scale.x = ref.current.scale.y = ref.current.scale.z = THREE.MathUtils.lerp(ref.current.scale.z, hovered ? 1.4 : 1, 0.1)
+//     ref.current.color.lerp(color.set(hovered ? 'red' : 'white'), hovered ? 1 : 0.1)
+//   })
+//   return (
+//     <group {...props}>
+//       <Instance ref={ref} onPointerOver={(e) => (e.stopPropagation(), setHover(true))} onPointerOut={(e) => setHover(false)} />
+//     </group>
+//   )
+// }
 
 function App() {
+  const { count } = useControls({
+    range: { value: 100, min: 0, max: 300, step: 10 },
+  });
+
   const ref = useRef();
   useEffect(() => {}, []);
-
   return (
-    <Canvas camera={{ fov: 100, position: [0, 0, 30] }}>
+    <Canvas
+      gl={{
+        antialias: true,
+      }}
+      camera={{ fov: 100, position: [0, 0, 30] }}
+    >
       <ambientLight intensity={0.01} />
       <pointLight distance={60} intensity={4} color="lightblue" />
       <spotLight
@@ -19,16 +67,9 @@ function App() {
         penumbra={1}
         color="red"
       />
-      {/* <mesh>
-        <planeGeometry args={[1000, 1000]} />
-        <meshStandardMaterial
-          color="#00ffff"
-          roughness={0.5}
-          depthTest={false}
-        />
-      </mesh> */}
-      <OrbitControls minPolarAngle={0} maxPolarAngle={Math.PI / 2} />
+
       <Experience />
+      <OrbitControls minPolarAngle={0} maxPolarAngle={Math.PI / 2} />
     </Canvas>
   );
 }
